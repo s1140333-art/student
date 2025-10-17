b66da5d1cf271e22e19f8d6fd70b49c81ae9e948
s1140333-art <s1140333@dmhs.kh.edu.tw>
2025-10-17 16:18:56 +0800

    Add files via upload

---
+++ HW2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 1515;
+    double height = 198.5;
+    std::string name = "Michael";
+    bool is_pass = true;
+    std::cout << "姓名:" << name << " 身高: " << height << std::endl;
+    std::cout << "年齡:" << age<< " 是否及格: " << is_pass << std::endl;
+    return 0;
+}
