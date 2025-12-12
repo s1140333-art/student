4b810c9d393753b9413a52c6d1f538ebb99b172c
s1140333-art <s1140333@dmhs.kh.edu.tw>
2025-12-12 16:14:29 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    double height, weight, bmi;
-
-    cout << "請輸入身高(cm): ";
-    cin >> height;
-
-    cout << "請輸入體重(kg): ";
-    cin >> weight;
-
-    // 將身高轉換為公尺
-    height = height / 100.0;
-
-    // 計算 BMI
-    bmi = weight / (height * height);
-
-    cout << "您的 BMI = " << bmi << endl;
-    
-    if (bmi < 18.5) {
-        cout << "體重過輕";
-    }else if (bmi < 24.9) {
-        cout << "體重正常";
-    }else {
-        cout << "體重過重";
+int main()
+{
+    int subject = 5;
+    int grade[5] = {100,100,100,100,100,};
+    int sum = 0;
+    for (int i=0;i<5;i++) {
+        sum = sum + grade[i];
     }
+    int average = sum/subject;
+    cout<<"段考的平均是:"<< average;
     return 0;
 }
